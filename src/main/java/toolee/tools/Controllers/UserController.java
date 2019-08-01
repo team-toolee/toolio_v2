f30677cd43e05890a26cf1f70a49a094f2964bd0
sauravka <sauravka@amazon.com>
2019-07-31 17:29:44 -0700

    discover page with selecting various locations

--- src/main/java/toolee/tools/Controllers/UserController.java
+++ src/main/java/toolee/tools/Controllers/UserController.java
@@ -44,6 +44,8 @@ public class UserController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    public String[] cities = {"All","Seattle", "Spokane","Tacoma", "Vancouver","Bellevue", "Kent", "Everett", "Renton", "Federal Way", "Kirkland",
+            "Auburn", "Shoreline"};
 
 
     @GetMapping("/profile")
@@ -80,6 +82,40 @@ public class UserController {
          */
         Category[] categories = Category.values();
         m.addAttribute("categories",categories);
+
+        m.addAttribute("cities",cities);
+        return "discover";
+    }
+
+    @PostMapping("/discover")
+    public String testing(Model m, Principal p, @RequestParam String city){
+        AppUser loggedInUser = userRepository.findByUsername(p.getName());
+        List<AppUser> usersInCity;
+        if(city.equals("All")){
+            usersInCity = (List)userRepository.findAll();
+        }
+        else{
+            usersInCity= userRepository.findByCity(city);
+        }
+
+        List<Tool> toolsInCity = new ArrayList<>();
+
+        for(AppUser user: usersInCity){
+            if(!user.getUsername().equals(loggedInUser.getUsername())){
+                for(Tool tool: user.getTools()){
+                    toolsInCity.add(tool);
+                }
+            }
+        }
+
+        m.addAttribute("toolsInCity", toolsInCity);
+        m.addAttribute("principal", loggedInUser);
+        /*
+        Add following for ajax search feature
+         */
+        Category[] categories = Category.values();
+        m.addAttribute("categories",categories);
+        m.addAttribute("cities",cities);
         return "discover";
     }
 
@@ -113,7 +149,7 @@ public class UserController {
         Map<String, MessageAttributeValue> smsAttributes =
                 new HashMap<>();
         //<set SMS attributes>
-        sendSMSMessage(snsClient, message, seller.getPhoneNumber(), smsAttributes);
+        sendSMSMessage(snsClient, message, "+1"+seller.getPhoneNumber(), smsAttributes);
     }
 
     public static void sendSMSMessage(AmazonSNSClient snsClient, String message,
